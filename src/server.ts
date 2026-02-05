dbdd325522594a7b9732cd00419d0fedede6afa4
Néo Carlier <neo.carlier2212@gmail.com>
2026-02-05 15:37:41 +0100

    serveur express

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,17 @@
+import express from 'express'
+
+const app=express();
+const port=3000;
+app.get('/',(req,res)=> {
+    res.send("Bienvenue sur mon serveur API");
+});
+
+app.listen(port, () =>{
+    console.log(`Serveur lancé sur http://localhost:${port}`);
+});
 function greet(name: string): string{
-    return "une phrase ${name}"
-}
+    return `une phrase ${name}`
+};
 
 console.log(greet("tom"))
+
