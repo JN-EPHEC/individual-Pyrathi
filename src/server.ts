451f222d5d491096ecf7c1ef97de03718f800281
Néo Carlier <neo.carlier2212@gmail.com>
2026-02-12 14:51:55 +0100

    TP2 points 1 et 2

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,24 @@
 import express from 'express'
 import userRoutes from './routes/userRoutes';
+import sequelize from './config/databse';
+import User from './models/User';
 
 const app=express();
 const port=3000;
 app.get('/',(req,res)=> {
     res.send("Bienvenue sur mon serveur API");
 });
+app.use('/api', userRoutes);
 
+sequelize.sync({ force: false }) // force: false évite de supprimer les données à chaque redémarrage
+    .then(() => {
+        console.log('Connexion à la base de données réussie et modèles synchronisés');
+        
+        // On ne lance l'écoute du serveur que si la DB est prête
+        app.listen(port, () => {
+            console.log(`Serveur lancé sur http://localhost:${port}`);
+        });
+    })
 app.get('/api/data',(req,res)=>{
     const etudiants = [
 { id: 1, nom: "Dupont", prenom: "Jean" },
@@ -22,10 +34,11 @@ app.get('/api/hello/:name',(req,res)=>{
     res.json({ "message": `Bonjour ${nom}`, "timestamp": timestamp });
 });
 
-app.use('/api', userRoutes);
 
-app.listen(port, () =>{
-    console.log(`Serveur lancé sur http://localhost:${port}`);
-});
+
+
+
+
+
 
 
