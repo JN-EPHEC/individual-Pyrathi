a9c9e29390d27420e0049ce0e8ef9f0365f98484
Néo Carlier <neo.carlier2212@gmail.com>
2026-03-03 13:39:30 +0100

    TP3_2.3

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,14 @@ import sequelize from './config/database';
 import User from './models/User';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
+import { requestLogger } from './middlewares/logger';
 
 
 const app=express();
 const port=3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+app.use(requestLogger);
 app.get('/',(req,res)=> {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
